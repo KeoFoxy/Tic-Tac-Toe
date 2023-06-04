23827814860373087f1cd39fab66f8822bfa758d
KeoFoxy <arasdiel68@gmail.com>
2023-06-04 20:12:28 +0300

    New Colors and gameplay Field

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumansTurn: Bool = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            LazyVGrid(columns: columns) {
+                ForEach(0..<9) { i in
+                    ZStack {
+                        Rectangle()
+                            .aspectRatio(1, contentMode: .fit)
+                            .foregroundColor(.clear)
+                            .background(FieldBackground)
+                            .cornerRadius(10)
+                        Image(systemName: moves[i]?.indicator ?? "")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.white)
+                    }
+                    .onTapGesture {
+                        moves[i] = Move(player: isHumansTurn ? .human : .AI, boardIndex: i)
+                        isHumansTurn.toggle()
+                    }
+                }
+            }
         }
         .padding()
+    }
+}
+
+enum Player {
+    case human, AI
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
---
+++ Tic-Tac-Toe/Tools/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  Tic-Tac-Toe
+//
+//  Created by Alexander Sorokin on 04.06.2023.
+//
+
+import SwiftUI
+
+let MoonLight: Color = Color("Moon Light")
+let Fuchsia: Color = Color("Fuchsia")
+let SkyBlue: Color = Color("SkyBlue")
+let SkyPurple: Color = Color("Sky Purple")
+let PaleYellow: Color = Color("Pale Yellow")
+let PeachOrange: Color = Color("Peach Orange")
+
+let FieldBackground = LinearGradient(colors: [PaleYellow, PeachOrange], startPoint: .topLeading, endPoint: .bottomTrailing)
