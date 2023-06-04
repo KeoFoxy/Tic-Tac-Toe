fafaa1271069521ed4b16405716cbf256fa4ee80
KeoFoxy <arasdiel68@gmail.com>
2023-06-04 18:46:24 +0300

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Alexander Sorokin on 04.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
