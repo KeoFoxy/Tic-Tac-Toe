b540f7ef792bf17c1eeec2a83e37a8f3ab1200a0
KeoFoxy <arasdiel68@gmail.com>
2023-06-06 21:00:49 +0300

    AI improvements

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -81,7 +81,48 @@ struct ContentView: View {
         return moves.contains(where: { $0?.boardIndex == index })
     }
     
+    // If Ai can win, then win
+    // If Ai can't win, then block
+    // If Ai can't block, then take middle square
+    // If Ai can't take middle square, then random available square
+    
     func determineAIMovePosition(in moves: [Move?]) -> Int {
+        
+        // If Ai can win, then win
+        let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        
+        let AIMoves = moves.compactMap { $0 }.filter { $0.player == .AI }
+        let AIPositions = Set(AIMoves.map { $0.boardIndex } )
+        
+        for pattern in winPatterns {
+            let winPositions = pattern.subtracting(AIPositions)
+            
+            if winPositions.count == 1 {
+                let isAvailable = !isSquareOccupied(in: moves, forIndex: winPositions.first!)
+                if isAvailable { return winPositions.first! }
+            }
+        }
+        // If Ai can't win, then block
+        let humanMoves = moves.compactMap { $0 }.filter { $0.player == .human }
+        let humanPositions = Set(humanMoves.map { $0.boardIndex } )
+        
+        for pattern in winPatterns {
+            let winPositions = pattern.subtracting(humanPositions)
+            
+            if winPositions.count == 1 {
+                let isAvailable = !isSquareOccupied(in: moves, forIndex: winPositions.first!)
+                if isAvailable { return winPositions.first! }
+            }
+        }
+        
+        // If Ai can't block, then take middle square
+        let centerSquare = 4
+        if !isSquareOccupied(in: moves, forIndex: centerSquare) {
+            return centerSquare
+        }
+        
+        
+        // If Ai can't take middle square, then random available square
         var movePositon = Int.random(in: 0..<9)
         
         while isSquareOccupied(in: moves, forIndex: movePositon) {
@@ -93,6 +134,7 @@ struct ContentView: View {
     
     func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
         let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        
         let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
         let playerPositions = Set(playerMoves.map { $0.boardIndex } )
         
