b72b109ef313713ddfd6f2b85e89d5f4b263712a
KeoFoxy <arasdiel68@gmail.com>
2023-06-08 14:33:17 +0300

    New colors and gradients

--- Tic-Tac-Toe/Tools/Colors.swift
+++ Tic-Tac-Toe/Tools/Colors.swift
@@ -7,6 +7,15 @@
 
 import SwiftUI
 
+let customRedColor: Color = Color(red: 232/255, green: 69/255, blue: 69/255)
+let customMagentaColor: Color = Color(red: 226/255, green: 109/255, blue: 246/255)
+let customCyanColor: Color = Color(red: 177/255, green: 252/255, blue: 248/255)
+let customYellowColor: Color = Color(red: 253/255, green: 237/255, blue: 140/255)
+let customPurpleColor: Color = Color(red: 77/255, green: 31/255, blue: 182/255)
+let customLightBlueColor: Color = Color(red: 105/255, green: 226/255, blue: 246/255)
+let customPaleYellowColor: Color = Color(red: 179/255, green: 177/255, blue: 149/255)
+let customPaleRedColor: Color = Color(red: 224/255, green: 118/255, blue: 129/255)
+
 let MoonLight: Color = Color("Moon Light")
 let Fuchsia: Color = Color("Fuchsia")
 let SkyBlue: Color = Color("SkyBlue")
@@ -15,3 +24,11 @@ let PaleYellow: Color = Color("Pale Yellow")
 let PeachOrange: Color = Color("Peach Orange")
 
 let FieldBackground = LinearGradient(colors: [PaleYellow, PeachOrange], startPoint: .topLeading, endPoint: .bottomTrailing)
+
+var AngGradietn: some View {
+    AngularGradient(gradient: Gradient(colors: [ customRedColor, customMagentaColor, customCyanColor, customYellowColor, customRedColor ]), center: .center, angle: .degrees(120)).ignoresSafeArea()
+}
+
+var LinGradient: some View {
+    LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0), Color.white.opacity(1)]), startPoint: .bottom, endPoint: .top).ignoresSafeArea()
+}
