2699376d846b701da9e437c8c5cd250becc77a37
KeoFoxy <arasdiel68@gmail.com>
2023-06-09 17:48:05 +0300

    ScoreView

--- Tic-Tac-Toe/CustomViews/ScoreView.swift
+++ Tic-Tac-Toe/CustomViews/ScoreView.swift
@@ -8,25 +8,52 @@
 import SwiftUI
 
 struct ScoreView: View {
-//    @ObservedObject var viewModel: ScoreViewModel.shared
+        //    @ObservedObject var viewModel: ScoreViewModel.shared
     @StateObject var viewModel = ScoreViewModel.shared
     
     var body: some View {
-        VStack {
-            Text("Wins: \(viewModel.wins)")
-                .font(.title)
-            
-            Text("Losses: \(viewModel.losses)")
-                .font(.title)
-            
-            Text("Draws: \(viewModel.draws)")
-                .font(.title)
+        ZStack {
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(Color.pink, lineWidth: 1)
+                .frame(width: 250, height: 200)
+                .foregroundColor(.clear)
+                .background(.white) //add cool gradiend
+                .blur(radius: 5)
+                .cornerRadius(10)
+            Score
+                .frame(width: 150)
+        }
+    }
+    
+    var Score: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            HStack {
+                Text("Wins:")
+                    .font(.custom("Poppins Bold", size: 24, relativeTo: .title))
+                Spacer()
+                Text("\(viewModel.wins)")
+                    .font(.custom("Poppins Bold", size: 24, relativeTo: .title))
+            }
+            HStack {
+                Text("Losses:")
+                    .font(.custom("Poppins Bold", size: 24, relativeTo: .title))
+                Spacer()
+                Text("\(viewModel.losses)")
+                    .font(.custom("Poppins Bold", size: 24, relativeTo: .title))
+            }
+            HStack {
+                Text("Draws:")
+                    .font(.custom("Poppins Bold", size: 24, relativeTo: .title))
+                Spacer()
+                Text("\(viewModel.draws)")
+                    .font(.custom("Poppins Bold", size: 24, relativeTo: .title))
+            }
         }
     }
 }
 
-//struct ScoreView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ScoreView(viewModel: <#ScoreViewModel#>)
-//    }
-//}
+struct ScoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScoreView(viewModel: ScoreViewModel.shared)
+    }
+}
