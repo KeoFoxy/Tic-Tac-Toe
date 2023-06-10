270196b5b04af7a4f6bb8931aa45e5eacabb7ead
KeoFoxy <arasdiel68@gmail.com>
2023-06-10 21:24:14 +0300

    Project Reorganization

---
+++ CustomViews/SettingsView.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsView.swift
+//  Tic-Tac-Toe
+//
+//  Created by Alexander Sorokin on 10.06.2023.
+//
+
+// TODO: Add settings for colors, difficulty, PvP
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
